c63d5f2501ecfa33464540b2dcec4850af9a6d1a
Moshiur Rahman <moshiurrahmanb11@gmail.com>
2021-10-27 19:15:04 +0600

    entering additional payment details with react

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -62,6 +62,18 @@ class OrdersController < ApplicationController
     end
   end
 
+  def pay_type_params
+    if order_params[:pay_type] == "Credit card"
+      params.require(:order).permit(:credit_card_number, :expiration_date)
+    elsif order_params[:pay_type] == "Check"
+      params.require(:order).permit(:routing_number, :account_number)
+    elsif order_params[:pay_type] == "Purchase order"
+      params.require(:order).permit(:po_number)
+    else
+      {}
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_order
@@ -72,10 +84,10 @@ class OrdersController < ApplicationController
     def order_params
       params.require(:order).permit(:name, :address, :email, :pay_type)
     end
-    private
-      def ensure_cart_isnt_empty
-        if @cart.line_items.empty?
-          redirect_to store_index_url, notice: 'Your cart is empty'
-        end
+
+    def ensure_cart_isnt_empty
+      if @cart.line_items.empty?
+        redirect_to store_index_url, notice: 'Your cart is empty'
       end
+    end
 end
