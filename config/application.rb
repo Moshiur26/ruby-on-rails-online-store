99f016a3abb4e0214e77e4870a88b0bbccee9296
Moshiur Rahman <moshiurrahmanb11@gmail.com>
2021-10-31 12:14:06 +0600

    add internationalization with locale switcher

--- config/application.rb
+++ config/application.rb
@@ -2,14 +2,18 @@ require_relative "boot"
 
 require "rails/all"
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
 
 module Depot
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+
+    config.middleware.use I18n::JS::Middleware
 
     # Configuration for the application, engines, and railties goes here.
     #
--- config/routes.rb
+++ config/routes.rb
@@ -10,13 +10,18 @@ Rails.application.routes.draw do
   get 'sessions/create'
   get 'sessions/destroy'
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
+  
   # get 'store/index'
-  root 'store#index', as: 'store_index'
+  # root 'store#index', as: 'store_index'
   resources :products do
     get :who_bought, on: :member
+  end
+
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index', via: :all
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
